3329e22144902605b72c6308ded2912c524e6855
Artur <arturbani@gmail.com>
2018-12-04 16:35:06 -0200

    version 8

--- practical-javascript/src/script.js
+++ practical-javascript/src/script.js
@@ -24,7 +24,8 @@ var todoList = {
     },
 
     toggleTodo: function(position){
-        this.todos[position].completed = !this.todos[position].completed;
+        var toggledTodo = !this.todos[position].completed;
+        this.todos[position].completed = toggledTodo;
         this.displayTodos();
 
     },
@@ -93,10 +94,39 @@ var handlers = {
     },
 
     addTodo: function(){
-        var todoTextInput = document.getElementById('todoTextInput');
-        todoList.addTodo(todoTextInput.value);
-        todoTextInput.value = '';
+        var addTodoTextInput = document.getElementById('addTodoTextInput');
+        todoList.addTodo(addTodoTextInput.value);
+        addTodoTextInput.value = '';
+
+    },
+
+    // Nas funções abaixo, a posição é subtraída de 1 antes de ser passada como parâmetro
+    // porque para facilitar interação do usuário, coloquei o valor mínimo nas opções de 1
+    // 
+    // Assim fica mais intuitivo, o usuário não vai pensar em colocar posição = 0, o programa
+    // trata isso.
+
+    changeTodo: function(){
+        var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+        var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+        todoList.changeTodo((changeTodoPositionInput.value-1), changeTodoTextInput.value);
+        changeTodoTextInput.value = '';
+        changeTodoPositionInput.value = '';
+ 
+    },
+
+    removeTodo: function(){
+        var removeTodoPositionInput = document.getElementById('removeTodoPositionInput');
+        todoList.removeTodo(removeTodoPositionInput.value-1);
+        removeTodoPositionInput.value = '';
+
+    },
+
+    toggleTodo: function(){
+        var toggleTodoPositionInput = document.getElementById('toggleTodoPositionInput');
+        todoList.toggleTodo(toggleTodoPositionInput.value-1);
+        toggleTodoPositionInput.value = '';
 
     }
-    
+
 };
